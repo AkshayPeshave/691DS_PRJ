cfab37be93f4bfe08ca43ae2b35b7562cdf253fd
Siraj Memon <siraj1@umbc.edu>
2016-10-07 14:37:49 -0400

    PostGIS SQL queries to map lat lng to region

---
+++ query/PostGIS_assign_latlng_region.sql
@@ -0,0 +1,13 @@
+﻿with cte as 
+(
+    select 1 as id, ST_Transform(ST_SetSRID(ST_MakePoint(-76.6129, 39.3106), 4326), 102685) point_location
+    ,st_astext(ST_Transform(ST_SetSRID(ST_MakePoint(-76.6129, 39.3106), 4326), 102685)) wkt 
+)
+SELECT a.id, b.csa2010
+FROM baltimore."vs14_housing" b, cte a 
+WHERE 
+ST_Intersects(a.point_location,  b.geom) 
+    or 
+ST_Contains(a.point_location, b.geom)
+    or
+ST_Within(a.point_location, b.geom)
---
+++ query/PostGIS_assign_latlng_region_1000.sql
@@ -0,0 +1,4 @@
+﻿SELECT a.id, b.csa2010
+FROM baltimore."vs14_housing" b, baltimore."tbl_331_1000_OnlyLatLng" a 
+WHERE 
+ST_Within(a.point_location, b.geom)
